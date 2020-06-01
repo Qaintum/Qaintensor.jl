c070456a94479f79100f47fc502808572a04ee7b
Esther Cruz <Esther.Cruz@physik.uni-muenchen.de>
2020-06-01 19:54:33 +0200

    added boundary conditions for the MPS generating function

--- src/mps.jl
+++ src/mps.jl
@@ -1,18 +1,53 @@
-function MPS(T::AbstractVector{Tensor})
-    l=length(T)
+function OpenMPS(T::AbstractVector{Tensor})
+    l = length(T)
     for i in 1:l
         @assert ndims(T[i]) == 3
     end
 
-    contractions=[Summation([i => 3, i+1 => 1]) for i in 1:l-1]
-    openidx=reverse([1 => 1; [i => 2 for i in 1:l]; l => 3])
-    tn=TensorNetwork(T, contractions, openidx)
+    contractions = [Summation([i => 3,i+1 => 1]) for i in 1:l-1]
+    openidx = reverse([1 => 1; [i => 2 for i in 1:l]; l => 3])
+    tn = TensorNetwork(T, contractions, openidx)
     return tn
 end
 
-function MPS(T::Tensor, l)
+function OpenMPS(T::Tensor, N::Integer)
     #translational invariant MPS
-    return MPS(fill(T, l))
+    return OpenMPS(fill(T, N))
+end
+
+function ClosedMPS(T::AbstractVector{Tensor})
+    l = length(T)
+    @assert ndims(T[1]) == 2
+    for i in 2:l-1
+        @assert ndims(T[i]) == 3
+    end
+     @assert ndims(T[l]) == 2
+
+    contractions = [Summation([1 => 2, 2 => 1]); [Summation([i => 3,i+1 => 1]) for i in 2:l-1]]
+    openidx = reverse([1 => 1; [i => 2 for i in 2:l]])
+    tn = TensorNetwork(T, contractions, openidx)
+    return tn
+end
+
+function ClosedMPS(T::Tensor, N::Integer)
+    return ClosedMPS(fill(T, N))
+end
+
+function PeriodicMPS(T::AbstractVector{Tensor})
+  l=length(T)
+    for i in 1:l
+        @assert Qaintensor.ndims(T[i]) == 3
+    end
+
+    contractions = [[Summation([i => 3,i+1 => 1]) for i in 1:l-1]; Summation([l => 3, 1 => 1])]
+    openidx = reverse([i => 2 for i in 1:l])
+    tn = TensorNetwork(T, contractions, openidx)
+    return tn
+end
+
+function PeriodicMPS(T::Tensor, N::Integer)
+    #translational invariant MPS
+    return PeriodicMPS(fill(T, N))
 end
 
 """
