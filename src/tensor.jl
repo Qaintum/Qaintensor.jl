eda1949b7ab73703d652e5c1b8b90a8927349049
Keefe Huang <huangq1234553@gmail.com>
2020-06-02 18:37:29 +0200

    Added method to find size of tensor dimensions

--- src/tensor.jl
+++ src/tensor.jl
@@ -2,9 +2,10 @@
 struct Tensor
 
     data::Array
-
     # TODO: add some meta-information like quantum numbers for tensor legs
 end
 
 
 ndims(t::Tensor) = Base.ndims(t.data)
+
+Base.size(t::Tensor) = Base.size(t.data)
