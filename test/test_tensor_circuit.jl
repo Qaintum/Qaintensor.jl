18f7857ad60bac129a282682b6cee060ee73d3e5
Keefe Huang <huangq1234553@gmail.com>
2020-06-10 16:29:40 +0200

    Update the tests to actually test the decompose function

--- test/test_tensor_circuit.jl
+++ test/test_tensor_circuit.jl
@@ -59,7 +59,7 @@ end
     cgc = CircuitGateChain{N}(cg)
     ψref = apply(cgc, contract(ψ)[:])
 
-    tensor_circuit!(ψ, cgc)
+    tensor_circuit!(ψ, cgc; is_decompose=true)
 
     @test ψref ≈ contract(ψ)[:]
 end
@@ -94,41 +94,7 @@ end
     ])
     ψref = apply(cgc, contract(ψ)[:])
 
-    tensor_circuit!(ψ, cgc)
+    tensor_circuit!(ψ, cgc; is_decompose=true)
 
     @test ψref ≈ contract(ψ)[:]
-end
-
-@testset ExtendedTestSet "test decompose" begin
-    # Test decomposition of 3-qubit gate
-    N = 4
-    # initial MPS wavefunction
-    ψ = TensorNetwork([], [], [])
-    push!(ψ.tensors, Tensor(randn(ComplexF64, (2, 6))))
-    push!(ψ.tensors, Tensor(randn(ComplexF64, (2, 6, 7))))
-    push!(ψ.tensors, Tensor(randn(ComplexF64, (2, 7, 5))))
-    push!(ψ.tensors, Tensor(randn(ComplexF64, (2, 5))))
-    # contract virtual legs
-    push!(ψ.contractions, Summation([1=>2, 2=>2]))
-    push!(ψ.contractions, Summation([2=>3, 3=>2]))
-    push!(ψ.contractions, Summation([3=>3, 4=>2]))
-
-    # open (physical) legs
-    push!(ψ.openidx, 1=>1)
-    push!(ψ.openidx, 2=>1)
-    push!(ψ.openidx, 3=>1)
-    push!(ψ.openidx, 4=>1)
-
-    cgc = CircuitGateChain{N}([
-    controlled_circuit_gate((3), (4,1), SwapGate(), N),
-    controlled_circuit_gate((1), (3), XGate(), N),
-    single_qubit_circuit_gate(2, YGate(), N),
-    single_qubit_circuit_gate(3, ZGate(), N),
-    ])
-    ψref = apply(cgc, contract(ψ)[:])
-
-    tensor_circuit!(ψ, cgc)
-
-    contract_order(ψ)
-    @test ψref ≈ contract(ψ, is_decompose=true)[:]
 end
