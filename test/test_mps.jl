9396d09d33660df9043ffafb7168fd45b7aed39d
Keefe Huang <huangq@in.tum.de>
2020-07-03 18:40:54 +0200

    Added tests for newest MPS functions

--- test/test_mps.jl
+++ test/test_mps.jl
@@ -2,6 +2,7 @@ using Test
 using TestSetExtensions
 using Qaintensor
 using LinearAlgebra
+using Random
 
 @testset ExtendedTestSet "open_mps" begin
 
@@ -44,4 +45,126 @@ end
     ψ = contract(mps)
 
     @test ψ_ref ≈ reshape(ψ.data, (2^N,))
+end
+
+@testset ExtendedTestSet "test permute MPS" begin
+    N = 6
+    b = AbstractArray{ComplexF64}[]
+    for i in 1:N
+        push!(b, randn(ComplexF64, (2)))
+    end
+
+    order = randperm(N)
+
+    ψ_ref = kron(b...)
+    ψ_perm = kron(b[order]...)
+
+    mps = MPS(ψ_ref)
+    permute!(mps, order)
+
+    ψ = contract(mps)
+    @test ψ_perm ≈ reshape(ψ.data, (2^N,))
+end
+
+@testset ExtendedTestSet "test permute MPS exceptions" begin
+
+    N = 6
+    b = AbstractArray{ComplexF64}[]
+    for i in 1:N
+        push!(b, randn(ComplexF64, (2)))
+    end
+
+    mps = MPS(kron(b...))
+
+    short_order = [1, 2, 3]
+    @test_throws ErrorException("Given permutation must be same length as number of Tensors in MPS") permute!(mps, short_order)
+
+    long_order = [1, 2, 3, 4, 5, 6, 7]
+    @test_throws ErrorException("Given permutation must be same length as number of Tensors in MPS") permute!(mps, short_order)
+
+    repeat_order = [1, 1, 1, 1, 1, 1]
+    @test_throws ErrorException("Permutation order cannot contain repeat values") permute!(mps, repeat_order)
+
+    negative_order = [1,-2, 3, 4, -5, 6]
+    @test_throws ErrorException("Permutation order can only contain positive values") permute!(mps, negative_order)
+
+    negative_order = [1, 2, 8, 4, 5, 6]
+    @test_throws ErrorException("Wire numbers in permutation order cannot exceed number of wires in MPS") permute!(mps, negative_order)
+end
+
+
+@testset ExtendedTestSet "test switch MPS qubits" begin
+    N = 6
+    b = AbstractArray{ComplexF64}[]
+    for i in 1:N
+        push!(b, randn(ComplexF64, (2)))
+    end
+    order = Int[1, 3, 2, 5, 6, 4]
+    ψ_ref = kron(b...)
+    ψ_perm = kron(b[order]...)
+
+    mps = MPS(ψ_ref)
+    switch!(mps, [(2,3), (4,6), (4,5)])
+
+    ψ = contract(mps)
+    @test ψ_perm ≈ reshape(ψ.data, (2^N,))
+
+    mps = MPS(ψ_ref)
+    switch!(mps, 2, 3)
+    switch!(mps, 4, 6)
+    switch!(mps, 4, 5)
+
+    ψ = contract(mps)
+    @test ψ_perm ≈ reshape(ψ.data, (2^N,))
+end
+
+@testset ExtendedTestSet "test switch MPS exceptions" begin
+
+    N = 6
+    b = AbstractArray{ComplexF64}[]
+    for i in 1:N
+        push!(b, randn(ComplexF64, (2)))
+    end
+
+    mps = MPS(kron(b...))
+
+    @test_throws ErrorException("Wire indices `i` and `j` must be positive") switch!(mps, 1, -2)
+    @test_throws ErrorException("Indices to swap `i` and `j` must be less than or equal to the number of open wires in MPS") switch!(mps, 7, 4)
+
+    @test_throws ErrorException("Wire indices `i` and `j` must be positive") switch!(mps, [(3, 4), (1, -2)])
+    @test_throws ErrorException("Indices to swap `i` and `j` must be less than or equal to the number of open wires in MPS") switch!(mps, [(9, 10), (1, -2)])
+end
+
+@testset ExtendedTestSet "test switch neighboring MPS qubits" begin
+    N = 6
+    b = AbstractArray{ComplexF64}[]
+    for i in 1:N
+        push!(b, randn(ComplexF64, (2)))
+    end
+    order = Int[1, 3, 4, 5, 6, 2]
+    ψ_ref = kron(b...)
+    ψ_perm = kron(b[order]...)
+
+    mps = MPS(ψ_ref)
+    switch!(mps, 4)
+    switch!(mps, 3)
+    switch!(mps, 2)
+    switch!(mps, 1)
+
+    ψ = contract(mps)
+    @test ψ_perm ≈ reshape(ψ.data, (2^N,))
+end
+
+@testset ExtendedTestSet "test switch neighboring MPS qubits exceptions" begin
+    N = 6
+    b = AbstractArray{ComplexF64}[]
+    for i in 1:N
+        push!(b, randn(ComplexF64, (2)))
+    end
+    ψ_ref = kron(b...)
+
+    mps = MPS(ψ_ref)
+
+    @test_throws BoundsError switch!(mps, 7)
+    @test_throws BoundsError switch!(mps, -2)
 end
