02aa1628d43e643ef3850ca5bc3f6780a6c8cfa3
Esther Cruz <Esther.Cruz@physik.uni-muenchen.de>
2020-08-10 14:34:19 +0200

    fix MPOs ordering

--- src/mpo.jl
+++ src/mpo.jl
@@ -40,7 +40,6 @@ struct MPO <: TensorNetwork
                 push!(dims, i)
                 push!(dims, i + M)
             end
-
             m = permutedims(m, dims)
             m = reshape(m, (4*bond_dim, :))
 
@@ -61,17 +60,22 @@ struct MPO <: TensorNetwork
                 push!(con, Summation([i => 4, i+1 => 1]))
                 bond_dim = new_bond_dim
             end
-            for i in 1:M-1
-                push!(openidx, M-i+1 => 3)
-            end
-            push!(openidx, 1 => 2)
+            push!(t, Tensor(reshape(m, (bond_dim, 2, 2))))
+
+            #output dimensions
             for i in 1:M-1
                 push!(openidx, M-i+1 => 2)
+                #push!(openidx, i => 2)
             end
             push!(openidx, 1 => 1)
 
-            push!(t, Tensor(reshape(m, (bond_dim, 2, 2))))
+            #input dimensions
 
+            for i in 1:M-1
+                push!(openidx, M-i+1 => 3)
+                #push!(openidx, i => 3)
+            end
+            push!(openidx, 1 => 2)
             else
             #one-qubit operator
             m = reshape(m, 2, 2)
@@ -91,6 +95,9 @@ function shift_summation(S::Summation, step)
    return Summation([S.idx[i].first + step => S.idx[i].second for i in 1:2])
 end
 
+function shift_pair(P::Pair, step)
+    return P.first + step => P.second
+end
 """
     circuit_MPO(mpo::MPO, iwire::NTuple{M, <:Integer}) where M
 
@@ -98,6 +105,7 @@ extends an operator `MPO` acting on `M` qudits into an operator acting on `N` qu
 """
 
 function circuit_MPO(mpo::MPO, iwire::NTuple{M, <:Integer}) where M
+    collect(iwire) == sort(collect(iwire)) || @error("Wires not sorted")
     N = length(iwire[1]:iwire[end])
     @assert length(mpo.tensors) == M
 
@@ -106,23 +114,30 @@ function circuit_MPO(mpo::MPO, iwire::NTuple{M, <:Integer}) where M
 
     # TODO: support general "qudits"
     d = 2
+    qwire = (iwire[1]:iwire[end]...,)
+    pipeswire = setdiff(qwire, iwire)
+    pipeswire = sort(pipeswire)
+    qwire = reverse(qwire)
 
-    pipeswire = setdiff((iwire[1]:iwire[end]...,), iwire)
-    for (i,w) in enumerate(pipeswire)
-        bond = size(mpo.tensors[w-iwire[1]])[end]
+    for (i,w) in enumerate(reverse(pipeswire))
+        ind = findfirst(x->x==w, qwire)
+        bond = size(mpo.tensors[ind-1])[end]
         Vpipe = reshape(kron(Matrix(1I, bond, bond), Matrix(1I, d, d)), (bond, d, bond, d))
-        #Julia ordena las dimensiones de salida->entrada
         Vpipe = permutedims(Vpipe, [1,2,4,3])
-        insert!(mpo.tensors, w-iwire[1]+1, Tensor(Vpipe))
+        insert!(mpo.tensors, ind, Tensor(Vpipe))
     end
 
     for i in M:N-1
         push!(mpo.contractions, Summation([i => 4, i+1 => 1]))
-        pushfirst!(mpo.openidx, i + 1 => 3)
-        insert!(mpo.openidx, M + i, i + 1 => 2)
+        pushfirst!(mpo.openidx, i + 1 => 2)
+        insert!(mpo.openidx, i+2, i + 1 => 3)
     end
-
     return mpo
+end
+
+function circuit_MPO(m::AbstractMatrix, iwire::NTuple{M, <:Integer}) where M
+    collect(iwire) == sort(collect(iwire)) || @error("Wires not sorted")
+return circuit_MPO(MPO(m), iwire)
 end
 
 """
@@ -132,43 +147,43 @@ given a state `ψ`  in a Tensor Network form and an operator `mpo` acting on `M`
 """
 
 function apply_MPO(ψ::TensorNetwork, mpo::MPO, iwire::NTuple{M, <:Integer}) where M
-
-    iwire = sort(collect(iwire))
-    # TODO: support general "qudits"
-    d = 2
     n = length(ψ.openidx) #number of qudits
     step = length(ψ.tensors)
+    qwire = (iwire[1]:iwire[end]...,)
+    N = length(qwire)
+    # TODO: support general "qudits"
+    d = 2
 
-    N = length(iwire[1]:iwire[end])
     if M < N
         mpo = circuit_MPO(mpo, Tuple(iwire))
     end
 
-    qwire = (iwire[1]:iwire[end]...,)
-    qbef = (1:iwire[1]-1...,)
-    qaft = (iwire[end]+1:n...,)
-
     ψ_prime = GeneralTensorNetwork([copy(ψ.tensors); copy(mpo.tensors)],
                         [copy(ψ.contractions); shift_summation.(mpo.contractions, step)],
-                        [])
-    for (i,w) in enumerate(qwire[2:end])
-        push!(ψ_prime.contractions, Summation([ψ.openidx[n-w+1], step+i+1 => 3]))
+                        copy(ψ.openidx))
+    #add the contractions between mpo and state
+    for (i,w) in enumerate(qwire)
+        push!(ψ_prime.contractions, Summation([ψ.openidx[w], shift_pair(mpo.openidx[i+N], step)]))
     end
-    push!(ψ_prime.contractions, Summation([ψ.openidx[n-qwire[1]+1], step + 1 => 2]))
-
-    for (i,q) in enumerate(qaft)
-        push!(ψ_prime.openidx, ψ.openidx[n-q+1])
+    #update the openidx
+    for (i,q) in enumerate(qwire)
+        ψ_prime.openidx[q] = shift_pair(mpo.openidx[i], step)
     end
-
-    for (i,w) in enumerate(qwire[2:end])
-            push!(ψ_prime.openidx,  N + step - i + 1 => 2)
-    end
-    push!(ψ_prime.openidx, step + 1 => 1)
-
-
-    for (i,q) in enumerate(qbef)
-        push!(ψ_prime.openidx, ψ.openidx[n-q+1])
-    end
-
     return ψ_prime
+end
+
+function apply_MPO(ψ::TensorNetwork, m::AbstractMatrix, iwire::NTuple{M, <:Integer}) where M
+    iwire_sorted = sort(collect(iwire))
+    if iwire_sorted != collect(iwire)
+        sort_wires = sortperm(collect(iwire))
+        perm = reverse([1:M...])
+        perm = perm[sort_wires]
+        perm = reverse(perm)
+        perm = [perm; perm.+M]
+
+        m = reshape(m, fill(2, 2M)...)
+        m = permutedims(m, perm)
+        m = reshape(m, (2^M, 2^M))
+    end
+    return apply_MPO(ψ, MPO(m), Tuple(iwire_sorted))
 end
--- test/test_mpo.jl
+++ test/test_mpo.jl
@@ -1,16 +1,12 @@
 using Test
 using TestSetExtensions
 using Qaintensor
+using Qaintessent
 using LinearAlgebra
+using Combinatorics
+using StatsBase: sample
 
 @testset ExtendedTestSet "mpo" begin
-
-    #Test for converting a random matrix into an MPO
-    M = 3
-    A = rand(ComplexF64, 2^M ,2^M)
-    A_MPO = MPO(copy(A))
-    @test reshape(contract(A_MPO), (2^M, 2^M)) ≈ A
-
 
     function randn_complex(size)
         return (randn(size)
@@ -22,35 +18,55 @@ using LinearAlgebra
     Ucnot13 = permutedims(Ucnot13, [2,1,3,5,4,6])
     Ucnot13 = reshape(Ucnot13, (8, 8));
 
+    #Test for CNOT
     # create random MPS tensors, using bond dimensions (1, 3, 4, 1)
     mps = [randn_complex((2, 3)), randn_complex((3, 2, 4)), randn_complex((4, 2))]
     ψ_mps = ClosedMPS(Tensor.(mps));
     ψ = reshape(contract(ψ_mps), 2^3);
-
-    #Test for CNOT
     ψ_mps_cnot = apply_MPO(ψ_mps, MPO(Ucnot), (1,3));
     @test reshape(contract(ψ_mps_cnot), 8) ≈ Ucnot13*ψ
 
-    #Test for 2-qubit gates acting on non-adjacent qubits
-    N, M = 3, 2
-    
-    iwire = (1,3)
-    wbef = (1:iwire[1]-1...,)
-    wmid = setdiff((iwire[1]:iwire[end]...,), iwire )
-    waft = (iwire[end]+1:N...,)
-    perm = [wbef...; wmid...; iwire...; waft...]
-    U = rand(ComplexF64, 2^M ,2^M)
+    #Test for Circuit_mpo
+    A = rand(ComplexF64, 2 ,2)
+    B = rand(ComplexF64, 2 ,2)
+    U1 = kron(kron(A, Matrix(1I, 2,2)), B)
+    U2 = kron(A,B)
+    @test contract(MPO(U1)) ≈ contract(circuit_MPO(U2, (1,3)))
 
-    ψ_mps_prime = apply_MPO(ψ_mps, MPO(U), (1,3));
+    #Test for M-qubit gates acting on non-adjacent qubits out of N
+    N, M = 7, 3
+    combi = collect(combinations(1:N, M));
+    for (s,com) in enumerate(combi)
+        iwires_sorted = Tuple(com)
 
-    U_MPO = circuit_MPO(MPO(U), iwire)
-    U_MPO = reshape(contract(U_MPO), 2^N, 2^N)
+        w = [1:N...]
+        iwires = sample(w, M, replace = false)
 
-    U = reshape(kron(U, Matrix(1I, 2^(N-M), 2^(N-M))), (fill(2, 2N)...,) );
-    U = permutedims(U, [perm; perm.+ N])
-    U = reshape(U, (2^N, 2^N));
+        A = rand(ComplexF64, 2^M ,2^M);
+        U, R = qr(A)
+        U = Array(U);
+        GU = MatrixGate(U);
 
-    @test U_MPO ≈ U
-    @test reshape(contract(ψ_mps_prime), 8) ≈ U*ψ
+        circuit_GU_sorted = CircuitGate(iwires_sorted,GU,N); #GATE
+        circuit_GU = CircuitGate(Tuple(iwires),GU,N)
+
+        #ARBITRARY INPUT STATE
+        mps = []
+        for i in 1:N-2
+        push!(mps, randn_complex((2, 2,2)))
+        end
+        pushfirst!(mps, randn_complex((2, 2)))
+        push!(mps, randn_complex((2, 2)))
+
+        ψ_mps = ClosedMPS(Tensor.(mps));
+        ψ = reshape(contract(ψ_mps), 2^N);
+
+        ψ_gate_sorted = apply(circuit_GU_sorted, ψ);
+        ψ_gate = apply(circuit_GU, ψ);
+        ψ_mpo_sorted = apply_MPO(ψ_mps, MPO(U), iwires_sorted);
+        ψ_mpo = apply_MPO(ψ_mps, U, Tuple(iwires));
+        @test reshape(contract(ψ_mpo), 2^N) ≈ ψ_gate
+        @test reshape(contract(ψ_mpo_sorted), 2^N) ≈ ψ_gate_sorted
+    end
 
 end
