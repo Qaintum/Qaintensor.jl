c488b4b31d39afb9c011c7cf12d8e81d4aa24b23
Keefe Huang <huangq@in.tum.de>
2020-08-25 16:07:11 +0200

    Shifted svd.jl tests to own file. Added contract_svd exception testing. Added tests for check_MPS

--- test/test_mps.jl
+++ test/test_mps.jl
@@ -4,25 +4,84 @@ using Qaintensor
 using LinearAlgebra
 using Random
 
-@testset ExtendedTestSet "contract_svd" begin
-    t1 = rand(ComplexF64, 4,4)
-    t2 = rand(ComplexF64, 4,4)
-    t = contract_svd(Tensor(t1),Tensor(t2), (2,1))
-    @test t.data ≈ t1*t2
+@testset ExtendedTestSet "contract_svd_mps" begin
+    T = Tensor(rand(2,2,2))
+    mps = OpenMPS(T, 3)
+    gmps = GeneralTensorNetwork(mps.tensors, mps.contractions, mps.openidx)
+    mps_contract = contract(gmps)
+    mps_svd = contract_svd_mps(mps; er=0.0)
 
-    t1 = Tensor(rand(ComplexF64, 2,3,4,2))
-    t2 = Tensor(rand(ComplexF64, 1,5,2,4))
+    @test mps_contract ≈ mps_svd
+    @test_throws ErrorException("Function doesn't support periodic boundary conditions for now") contract_svd_mps(PeriodicMPS(T, 3); er=0.0)
+end
 
-    con1 = Summation([1 => 1, 2 => 3])
-    con2 = Summation([1 => 3, 2 => 4])
-    open1 = reverse([1 => 2, 1 => 3, 1 => 4, 2 => 1, 2 => 2, 2 => 4])
-    open2 = reverse([1 => 1, 1 => 2, 1 => 4, 2 => 1, 2 => 2, 2 => 3])
+@testset ExtendedTestSet "check_mps exceptions" begin
+    T = Tensor(rand(2,2,2))
+    mps = OpenMPS(T, 3)
+    mps.contractions[1] = Summation([1=>2, 2=>1])
+    @test_throws ErrorException("Tensor objects first leg must contract with last leg of previous Tensor object") check_mps(mps)
 
-    tn1 = GeneralTensorNetwork([t1,t2], [con1], open1)
-    tn2 = GeneralTensorNetwork([t1,t2], [con2], open2)
+    mps.contractions[1] = Summation([1=>3, 2=>2])
+    @test_throws ErrorException("Tensor objects last leg must contract with first leg of next Tensor object") check_mps(mps)
 
-    @test contract(tn1) ≈ contract_svd(t1, t2, (1,3)).data
-    @test contract(tn2) ≈ contract_svd(t1, t2, (3,4)).data
+    mps.tensors[2] = Tensor(rand(2,2,2,2))
+    @test_throws ErrorException("Each Tensor object in MPS form can only have 2 or 3 legs") check_mps(mps)
+end
+
+@testset ExtendedTestSet "conversion from state-vector to mps" begin
+    N = 5
+    b1 = randn(ComplexF64, (2))
+    b2 = randn(ComplexF64, (2))
+    b3 = randn(ComplexF64, (2))
+    b4 = randn(ComplexF64, (2))
+    b5 = randn(ComplexF64, (2))
+
+    ψ_ref = kron(b1,b2,b3,b4,b5)
+
+    mps = MPS(ψ_ref)
+
+    ψ = contract(mps)
+
+    @test ψ_ref ≈ reshape(ψ, (2^N,))
+end
+
+@testset ExtendedTestSet "conversion from state-vector to mps exceptions" begin
+    ψ = randn(ComplexF64, 6)
+
+    @test_throws ErrorException("Input state must have length 2^N") MPS(ψ)
+end
+
+@testset ExtendedTestSet "mps constructor" begin
+    N = 3
+
+    tensors = Tensor[]
+    push!(tensors, Tensor(randn(ComplexF64, (2, 6))))
+    push!(tensors, Tensor(randn(ComplexF64, (2, 6, 7))))
+    push!(tensors, Tensor(randn(ComplexF64, (2, 7))))
+
+    # contract virtual legs
+    contractions = Summation[]
+    push!(contractions, Summation([1=>2, 2=>2]))
+    push!(contractions, Summation([2=>3, 3=>2]))
+
+    # open (physical) legs
+    openidx = Pair[]
+    push!(openidx, 1=>1)
+    push!(openidx, 2=>1)
+    push!(openidx, 3=>1)
+
+    # initial MPS wavefunction
+    ψ = GeneralTensorNetwork(tensors, contractions, openidx)
+
+    cgc = qft_circuit(N)
+
+    # conventional statevector representation, as reference
+    ψref = apply(cgc, contract(ψ)[:])
+
+    # using tensor network representation
+    tensor_circuit!(ψ, cgc)
+
+    @test ψref ≈ contract(ψ)[:]
 end
 
 @testset ExtendedTestSet "open_mps" begin
@@ -55,23 +114,6 @@ end
     T = Tensor(rand(2,2,2))
     mps = PeriodicMPS(T, 3)
     @test_throws ErrorException contract_svd_mps(mps, er=0.0)
-end
-
-@testset ExtendedTestSet "conversion from state-vector to mps" begin
-    N = 5
-    b1 = randn(ComplexF64, (2))
-    b2 = randn(ComplexF64, (2))
-    b3 = randn(ComplexF64, (2))
-    b4 = randn(ComplexF64, (2))
-    b5 = randn(ComplexF64, (2))
-
-    ψ_ref = kron(b1,b2,b3,b4,b5)
-
-    mps = MPS(ψ_ref)
-
-    ψ = contract(mps)
-
-    @test ψ_ref ≈ reshape(ψ, (2^N,))
 end
 
 @testset ExtendedTestSet "test permute MPS" begin
@@ -155,11 +197,11 @@ end
 
     mps = MPS(kron(b...))
 
-    @test_throws ErrorException("Wire indices `i` and `j` must be positive") switch!(mps, 1, -2)
-    @test_throws ErrorException("Indices to swap `i` and `j` must be less than or equal to the number of open wires in MPS") switch!(mps, 7, 4)
+    @test_throws ErrorException("Wire indices `i` and `j` must be positive") switch!(deepcopy(mps), 1, -2)
+    @test_throws ErrorException("Indices to swap `i` and `j` must be less than or equal to the number of open wires in MPS") switch!(deepcopy(mps), 7, 4)
 
-    @test_throws ErrorException("Wire indices `i` and `j` must be positive") switch!(mps, [(3, 4), (1, -2)])
-    @test_throws ErrorException("Indices to swap `i` and `j` must be less than or equal to the number of open wires in MPS") switch!(mps, [(9, 10), (1, -2)])
+    @test_throws ErrorException("Wire indices `i` and `j` must be positive") switch!(deepcopy(mps), [(3, 4), (1, -2)])
+    @test_throws ErrorException("Indices to swap `i` and `j` must be less than or equal to the number of open wires in MPS") switch!(deepcopy(mps), [(9, 10), (1, -2)])
 end
 
 @testset ExtendedTestSet "test switch neighboring MPS qubits" begin
@@ -194,15 +236,4 @@ end
 
     @test_throws BoundsError switch!(mps, 7)
     @test_throws BoundsError switch!(mps, -2)
-end
-
-@testset ExtendedTestSet "contract_svd_mps" begin
-    T = Tensor(rand(2,2,2))
-    mps = OpenMPS(T, 3)
-    gmps = GeneralTensorNetwork(mps.tensors, mps.contractions, mps.openidx)
-    mps_contract = contract(gmps)
-    mps_svd = contract_svd_mps(mps; er=0.0)
-
-    @test mps_contract ≈ mps_svd
-    @test_throws ErrorException("Function doesn't support periodic boundary conditions for now") contract_svd_mps(PeriodicMPS(T, 3); er=0.0)
 end
---
+++ test/test_svd.jl
@@ -0,0 +1,50 @@
+using Test
+using TestSetExtensions
+using Qaintensor
+using LinearAlgebra
+using Random
+
+@testset ExtendedTestSet "contract_svd" begin
+    t1 = rand(ComplexF64, 4,4)
+    t2 = rand(ComplexF64, 4,4)
+    t = contract_svd(Tensor(t1),Tensor(t2), (2,1))
+    @test t.data ≈ t1*t2
+
+    t1 = Tensor(rand(ComplexF64, 2,3,4,2))
+    t2 = Tensor(rand(ComplexF64, 1,5,2,4))
+
+    con1 = Summation([1 => 1, 2 => 3])
+    con2 = Summation([1 => 3, 2 => 4])
+    open1 = reverse([1 => 2, 1 => 3, 1 => 4, 2 => 1, 2 => 2, 2 => 4])
+    open2 = reverse([1 => 1, 1 => 2, 1 => 4, 2 => 1, 2 => 2, 2 => 3])
+
+    tn1 = GeneralTensorNetwork([t1,t2], [con1], open1)
+    tn2 = GeneralTensorNetwork([t1,t2], [con2], open2)
+
+    @test contract(tn1) ≈ contract_svd(t1, t2, (1,3)).data
+    @test contract(tn2) ≈ contract_svd(t1, t2, (3,4)).data
+end
+
+@testset ExtendedTestSet "contract_svd exceptions" begin
+    t1 = rand(ComplexF64, 4,4)
+    t2 = rand(ComplexF64, 4,4)
+    t = contract_svd(Tensor(t1),Tensor(t2), (2,1))
+    @test t.data ≈ t1*t2
+
+    t1 = Tensor(rand(ComplexF64, 2,3,4,2))
+    t2 = Tensor(rand(ComplexF64, 1,5,2,4))
+
+    con1 = Summation([1 => 1, 2 => 3])
+    con2 = Summation([1 => 3, 2 => 4])
+    open1 = reverse([1 => 2, 1 => 3, 1 => 4, 2 => 1, 2 => 2, 2 => 4])
+    open2 = reverse([1 => 1, 1 => 2, 1 => 4, 2 => 1, 2 => 2, 2 => 3])
+
+    tn1 = GeneralTensorNetwork([t1,t2], [con1], open1)
+    tn2 = GeneralTensorNetwork([t1,t2], [con2], open2)
+
+    @test_throws ErrorException("Error must be positive") contract(tn1) ≈ contract_svd(t1, t2, (1,3); er=-0.3).data
+
+    t2_short = Tensor(rand(ComplexF64, 8,8))
+    @test_throws ErrorException("Dimensions of contraction legs do not match") contract(tn1) ≈ contract_svd(t1, t2, (1,3)).data
+    @test contract(tn2) ≈ contract_svd(t1, t2_short, (3,4)).data
+end
