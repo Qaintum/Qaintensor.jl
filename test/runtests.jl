6fdffab8ac86589c8e827754340376ec2d39660c
Keefe Huang <huangq@in.tum.de>
2020-09-03 13:53:55 +0200

    Let flip_qubit_order use the correct branch of Qaintessent

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Test
 using TestSetExtensions
 import Pkg
 
-Pkg.add(Pkg.PackageSpec(url="https://github.com/Qaintum/Qaintessent.jl"))
+Pkg.add(Pkg.PackageSpec(url="https://github.com/Qaintum/Qaintessent.jl", rev="flip_qubit_order"))
 
 @testset "All the tests" begin
     @includetests ARGS
