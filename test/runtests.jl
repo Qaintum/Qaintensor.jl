026150a0b373fc64291164fbd970ff128ecdce93
Qunsheng Huang <keefe.huang@tum.de>
2020-09-17 16:30:10 +0200

    Try using try/catch to see if something needs to be installed

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,11 @@ using Test
 using TestSetExtensions
 import Pkg
 
-Pkg.add(Pkg.PackageSpec(url="https://github.com/Qaintum/Qaintessent.jl"))
-
+try
+    Pkg.instantiate()
+catch e
+    Pkg.add(Pkg.PackageSpec(url="https://github.com/Qaintum/Qaintessent.jl"))
+end
 @testset "All the tests" begin
     @includetests ARGS
 end
