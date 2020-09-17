8d7b7487872ff335518c8596b34976a20204faef
Qunsheng Huang <keefe.huang@tum.de>
2020-09-17 16:39:54 +0200

    Remove unneeded installation lines in runtest.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,6 @@ using Test
 using TestSetExtensions
 import Pkg
 
-try
-    Pkg.instantiate()
-catch e
-    Pkg.add(Pkg.PackageSpec(url="https://github.com/Qaintum/Qaintessent.jl"))
-end
 @testset "All the tests" begin
     @includetests ARGS
 end
