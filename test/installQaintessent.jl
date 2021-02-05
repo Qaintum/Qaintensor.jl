28b19bc18de71fb2a7e7cdd9164efcb93931bd4c
Qunsheng Huang <keefe.huang@tum.de>
2021-02-05 15:09:18 +0100

    Try catch remove-install of Qaintessent/Qaintmodels

--- test/installQaintessent.jl
+++ test/installQaintessent.jl
@@ -1,4 +1,11 @@
 import Pkg
 
+try
+    Pkg.rm(Pkg.PackageSpec(url="https://github.com/Qaintum/Qaintessent.jl"))
+    Pgk.rm(Pkg.PackageSpec(url="https://github.com/Qaintum/Qaintmodels.jl"))
+catch
+end
+
+
 Pkg.add(url="https://github.com/Qaintum/Qaintessent.jl")
 Pkg.add(url="https://github.com/Qaintum/Qaintmodels.jl")
