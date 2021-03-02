0aab1b06bea3fad1d148bccb590e384ccb468c96
Qunsheng Huang <keefe.huang@tum.de>
2021-03-02 16:42:19 +0100

    Remove installation of dependencies in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,6 @@
 using Test
 using TestSetExtensions
-import Pkg
 
-Pkg.add(Pkg.PackageSpec(url="https://github.com/Qaintum/Qaintessent.jl"))
-Pkg.add(Pkg.PackageSpec(url="https://github.com/Qaintum/Qaintmodels.jl"))
 
 @testset "All the tests" begin
     @includetests ARGS
