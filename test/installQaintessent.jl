f304fa82a5e5bf3184ead2772ec57b41dfb095e1
Qunsheng Huang <keefe.huang@tum.de>
2021-03-16 14:44:55 +0100

    Remove Qaintessent install from script, now registered

--- test/installQaintessent.jl
+++ test/installQaintessent.jl
@@ -1,11 +1,9 @@
 import Pkg
 
 try
-    Pkg.rm(Pkg.PackageSpec(url="https://github.com/Qaintum/Qaintessent.jl"))
     Pgk.rm(Pkg.PackageSpec(url="https://github.com/Qaintum/Qaintmodels.jl"))
 catch
 end
 
 
-Pkg.add(url="https://github.com/Qaintum/Qaintessent.jl")
 Pkg.add(url="https://github.com/Qaintum/Qaintmodels.jl")
